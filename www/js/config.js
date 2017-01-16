51c2d8a3d3ebe9c75b67f927b44547859bca4780
rtfuzz <Fuzz@Afonsos-MacBook-Pro.local>
2017-01-16 16:00:17 -0200

    js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 /*Kartero App Configuration*/
 var krms_driver_config ={		
-	'ApiUrl':"",					
-	'DialogDefaultTitle':"Kartero",	
-	'PushProjectID':"",	
-	'APIHasKey':""
+	'ApiUrl':"http://loadme.cyberomnes.com/api",					
+	'DialogDefaultTitle':"LoadMe",	
+	'PushProjectID':"172997422108",	
+	'APIHasKey':"@La2483801317"
 };
